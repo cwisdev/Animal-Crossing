33311e62fb4956a7205d1e66c9e3ea482f56bf6b
cwisdev <hunter.chris.dev@gmail.com>
2026-02-16 20:38:06 -0700

    Improvements to day/night cycle

--- Assets/DayNightManager.cs
+++ Assets/DayNightManager.cs
@@ -2,12 +2,13 @@ using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class SunController : MonoBehaviour
+public class DayNightManager: MonoBehaviour
 {
     public GameObject sun;
     public GameObject moon;
     public Color twilightColor = Color.orange;
     public Color dayColour = Color.white;
+    public Color nightColour = Color.blue;
     public float dayIntensity = 2;
     public float nightIntensity = .5f;
     public float latitude = 51.05f;
@@ -25,19 +26,17 @@ public class SunController : MonoBehaviour
     [Range(1,365)]
     public int currDay;
     public bool UseSystemTime;
+
+    int lastDay, lastHour, lastMinute;
     
     float sunrise, sunset;
     float currTime;
-
-    //DateTime time;
 
     private Light sunlight;
     private Light moonlight;
 
     void Start()
     {
-        //time = DateTime.Now;
-        UpdateDay();
         sunlight = sun.GetComponent<Light>();
         moonlight = moon.GetComponent<Light>();
     }
@@ -52,24 +51,36 @@ public class SunController : MonoBehaviour
             currDay = time.DayOfYear;
         }
 
-        UpdateDay();
+        // If this is a new day, get the new sunrise and sunset times
+        if (DateHasChanged())
+            CalcSunriseSunset();
 
-        float dayLength = sunset - sunrise;
-        currTime = currHour + ((float)currMinute / 60);
+        // Change the positions and lighting of the sun and moon when the time changes
+        if (TimeHasChanged())
+        {
+            float dayLength = sunset - sunrise;
+            currTime = currHour + ((float)currMinute / 60);
 
-        float rotateX = -90;
-        if ((currTime >= sunrise) && (currTime <= sunset))
-            rotateX = ((currTime - sunrise) / dayLength) * 180;
+            float rotateX = -90;
+            if ((currTime >= sunrise) && (currTime <= sunset))
+                rotateX = ((currTime - sunrise) / dayLength) * 180;
 
-        sun.transform.rotation = Quaternion.Euler(rotateX, 0, 0);
-        sunlight.color = GetSunColour();
-        sunlight.intensity = GetSunIntensity();
+            sun.transform.rotation = Quaternion.Euler(rotateX, 0, 0);
 
-        UpdateMoonRotation();
-        UpdateMoonIntensity();
+            UpdateSunColour();
+            UpdateSunIntensity();
+            UpdateMoonRotation();
+            UpdateMoonIntensity();
+            moonlight.color = nightColour;
+        }
+
+        lastDay = currDay;
+        lastHour = currHour;
+        lastMinute = currMinute;
     }
 
-    private Color GetSunColour()
+    // Transition between the daylight hue and the dusk/dawn hue
+    private void UpdateSunColour()
     {
         Color sunColour = dayColour;
         float twilightProgress;
@@ -85,10 +96,11 @@ public class SunController : MonoBehaviour
             sunColour = Color.Lerp(dayColour, twilightColor, twilightProgress);
         }
 
-        return sunColour;
+        sunlight.color = sunColour;
     }
 
-    private float GetSunIntensity()
+    // Increase or dims the moon's light during dawn and dusk
+    private void UpdateSunIntensity()
     {
         float sunIntensity;
         float twilightProgress;
@@ -108,17 +120,18 @@ public class SunController : MonoBehaviour
         else
             sunIntensity = dayIntensity;
 
-        return sunIntensity;
+        sunlight.intensity = sunIntensity;
     }
 
     private void UpdateMoonRotation()
     {
-        float moonRotateX, moonRotateY;
+        float rotateX, rotateY;
         float minMoonYaw = moonApexYaw - moonYawRange / 2;
         float maxMoonYaw = moonApexYaw + moonYawRange / 2;
 
         if ((currTime < sunrise) || (currTime > sunset))
         {
+            // Get the current progress of the night as a percentage
             float nightLength = 24 - (sunset - sunrise);
             float nightTime;
             if (currTime < sunrise)
@@ -127,26 +140,32 @@ public class SunController : MonoBehaviour
                 nightTime = currTime - sunset;
             nightTime /= nightLength;
 
-
+            // Arc the moon across the sky
             float midpoint = (maxMoonHeight + minMoonHeight) / 2;
             float amplitude = (maxMoonHeight - minMoonHeight) / 2;
-            moonRotateX = midpoint + amplitude * Mathf.Cos(2 * Mathf.PI * nightTime);
-
-            moonRotateY = minMoonYaw + (moonYawRange * nightTime);
+            rotateX = midpoint + amplitude * Mathf.Cos(2 * Mathf.PI * nightTime);
+            rotateY = minMoonYaw + (moonYawRange * nightTime);
         }
         else
         {
-            moonRotateX = maxMoonHeight;
-            moonRotateY = minMoonYaw;
+            rotateX = maxMoonHeight;
+
+            // Snap the moon to the east or west to ease the transition between dusk and dawn
+            if (currTime < 12)
+                rotateY = maxMoonYaw;
+            else
+                rotateY = minMoonYaw;
         }
 
-        moon.transform.rotation = Quaternion.Euler(moonRotateX, moonRotateY, 0);
+        moon.transform.rotation = Quaternion.Euler(rotateX, rotateY, 0);
     }
 
+    // Dims or increases the moon's light during dawn and dusk
     private void UpdateMoonIntensity()
     {
-        float twilightProgress;
         float moonIntensity;
+        float twilightProgress;
+
         if ((currTime > sunrise) && (currTime < sunrise + twilightIntensityBuffer))
         {
             twilightProgress = (currTime - sunrise) / twilightIntensityBuffer;
@@ -165,7 +184,17 @@ public class SunController : MonoBehaviour
         moonlight.intensity = moonIntensity;
     }
 
-    private void UpdateDay()
+    private bool TimeHasChanged()
+    {
+        return (currHour != lastHour) || (currMinute != lastMinute);
+    }
+
+    private bool DateHasChanged()
+    {
+        return (currDay != lastDay);
+    }
+
+    private void CalcSunriseSunset()
     {
         float declination = 23.44f * Mathf.Deg2Rad * Mathf.Sin(Mathf.Deg2Rad * (360f / 365f) * (currDay - 81));
         float hourAngle = Mathf.Acos(-Mathf.Tan(latitude * Mathf.Deg2Rad) * Mathf.Tan(declination));
